8a0a43aa4924cd17419ef7233d399b1b14d2da24
rubenSinzig <ruben.sinzig@posteo.de>
2021-10-28 11:24:45 +0200

    Contact Form -> change position && sizes

--- client/src/components/ContactForm.js
+++ client/src/components/ContactForm.js
@@ -5,7 +5,6 @@ const ContactForm = () => {
     <div className="wrapper-contact">
       {/* Form */}
       <div className="form-container">
-        <div className="blur"></div>
         <form>
           <label>
             <h5>Name</h5>
@@ -29,15 +28,6 @@ const ContactForm = () => {
 
           <input className="form-btn" type="submit" value="Send" />
         </form>
-      </div>
-
-      {/* Map */}
-      <div className="map-contaier">
-        <iframe
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2588.2740761101745!2d8.065421315902478!3d49.55485157936184!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47963cbd09e0582d%3A0x682f70d2122924b0!2sCasa%20Verde!5e0!3m2!1sde!2sde!4v1635237432543!5m2!1sde!2sde"
-          width="300"
-          height="300"
-        ></iframe>
       </div>
     </div>
   );
