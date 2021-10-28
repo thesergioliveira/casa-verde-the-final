a728ed8ebe9d2bae899bec0c8b85afba903062bd
rubenSinzig <ruben.sinzig@posteo.de>
2021-10-28 11:32:45 +0200

    Map -> change position && sizes

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -5,8 +5,8 @@ const Map = () => {
     <div className="map-contaier">
       <iframe
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2588.2740761101745!2d8.065421315902478!3d49.55485157936184!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47963cbd09e0582d%3A0x682f70d2122924b0!2sCasa%20Verde!5e0!3m2!1sde!2sde!4v1635237432543!5m2!1sde!2sde"
-        width="300"
-        height="300"
+        width="90%"
+        height="300px"
       ></iframe>
     </div>
   );
