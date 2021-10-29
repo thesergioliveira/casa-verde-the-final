e3faa1871a975722c4bc45ff6f4ac7923cb2a0c6
rubenSinzig <ruben.sinzig@posteo.de>
2021-10-29 14:01:59 +0200

    install react icons && add pre-styling

--- client/src/components/Shop/ShopLandingpage.js
+++ client/src/components/Shop/ShopLandingpage.js
@@ -1,18 +1,25 @@
 import React, { useState } from "react";
 import ShopSlider from "../../shopSlider.json";
+import { MdArrowBackIos, MdArrowForwardIos } from "react-icons/md";
 
 const ShopLandingpage = () => {
   const slider = ShopSlider.map((obj) => {
     const { id, name, alt, img } = obj;
 
     return (
-      <div className="slide">
+      <div key={id} className="slide">
         <img src={process.env.PUBLIC_URL + img} alt={alt} />
       </div>
     );
   });
 
-  return <div className="slider-wrapper">{slider}</div>;
+  return (
+    <div className="slider-wrapper">
+      {slider}
+      <MdArrowBackIos className="silder-icon left" />
+      <MdArrowForwardIos className="silder-icon right" />
+    </div>
+  );
 };
 
 export default ShopLandingpage;
