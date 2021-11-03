fdc195bfa804d0d4b54d2103d4cbb6d8e4451fdb
angelo.p <mrpalaiologos@gmail.com>
2021-11-03 09:27:09 +0100

    searchbar ready

--- client/src/App.js
+++ client/src/App.js
@@ -19,34 +19,6 @@ import { DataProvider } from "./components/Context";
 function App() {
   return (
     <DataProvider>
-<<<<<<< HEAD
-    <Router
-     
-    >
-      <Nav />
-      <main>
-        <p>
-          <b>**Mobile page is under construction**</b>
-        </p>
-        <Switch>
-          {/* Need to add public.env */}
-          <Route path="/" exact render={Home}></Route>
-          <Route path="/shop" exact component={Shop}></Route>
-          <Route path="/services" exact render={Services}></Route>
-          <Route path="/events" exact render={Events}></Route>
-          <Route path="/rent" exact render={Rent}></Route>
-          <Route path="/ballet-pilates" exact render={BalletAndPilates}></Route>
-          <Route path="/technic" exact render={TechnicalConsulting}></Route>
-          <Route path="/about" exact render={About}></Route>
-          <Route path="/contact" exact render={Contact}></Route>
-          <Route path="/login" exact component={Login}></Route>
-          <Route path="/register" exact component={Register}></Route>
-          <Route path="/basket" exact component={Basket}></Route>
-        </Switch>
-      </main>
-      <Footer />
-    </Router></DataProvider>
-=======
       <Router>
         <Nav />
         <main>
@@ -73,7 +45,6 @@ function App() {
         <Footer />
       </Router>
     </DataProvider>
->>>>>>> caa0a87e122caec20e24bec41fe745262563e08c
   );
 }
 
