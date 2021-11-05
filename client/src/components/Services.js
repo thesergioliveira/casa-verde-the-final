96083abdf8f14beedb0ec82e5b746a625f26bc4d
iulianSta <iulianstangithub@gmail.com>
2021-11-05 15:22:59 +0100

    fix issues

--- client/src/components/Services.js
+++ client/src/components/Services.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+//import { Link } from "react-router-dom";
 import ServicesData from "../JSON/services.json";
 import ContactInformation from "./ContactInformation";
 import ContactForm from './ContactForm';
