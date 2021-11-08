f57bceedfc6ca0e9c1e7312be7a7582e9e4c8a56
rubenSinzig <ruben.sinzig@posteo.de>
2021-11-08 11:45:41 +0100

    create component for gallery -> Weddings -> set key && id

--- client/src/components/Gallery/Weddings.js
+++ client/src/components/Gallery/Weddings.js
@@ -7,22 +7,24 @@ const Weddings = () => {
 
   const weddings = ServicesData.filter((obj) => obj.name === "Weddings");
 
-  // Show more images onClick
+  // onClick show more images
   const showMoreImg = () => {
     setImages(images + 6);
   };
 
-  // Show image gallery with header and button
+  // Category --> heading, images and button
   const weddingGallery = weddings.map((obj) => {
-    const { id, name, linkId, imgs } = obj;
+    const { name, linkId, imgs } = obj;
 
     return (
-      <div className="container-gallery">
-        {/* header image gallery */}
-        <h3 key={uuidv4()}>{name}</h3>
+      <div key={uuidv4()} className="container-gallery">
+        {/* Heading for category */}
+        <h3 id={linkId} key={uuidv4()}>
+          {name}
+        </h3>
 
-        {/* show images */}
-        <div className="container-img-outer">
+        {/* Images gallery */}
+        <div key={uuidv4()} className="container-img-outer">
           {imgs.map((img) => {
             if (imgs.indexOf(img) <= images) {
               return (
@@ -34,7 +36,7 @@ const Weddings = () => {
           })}
         </div>
 
-        {/* button to show more images */}
+        {/* Button --> Show more */}
         <button key={uuidv4()} onClick={showMoreImg}>
           Show more
         </button>
