dcb58954c5f28d689ea4700812796dd8400ed628
iulianSta <iulianstangithub@gmail.com>
2021-11-18 16:12:42 +0100

    fixing conflicts inside basket

--- client/src/components/Shop/Basket.js
+++ client/src/components/Shop/Basket.js
@@ -39,19 +39,7 @@ const Basket = () => {
 
   return (
     <div className="main-basket-container">
-<<<<<<< HEAD
-      <span>
-        <h1> Shopping Basket</h1>
-        <p className="popup-message">
-          {" "}
-          <i>&nbsp;&nbsp;!&nbsp;&nbsp;</i>&nbsp;
-          Bitte beachte das nicht alle Artikel versendet werden können!
-        </p>
-        <h2> welcome {data?.username} </h2>
-      </span>
-=======
       <h1>Shopping Basket</h1>
->>>>>>> cd274e9fce30fdf01da27766eea4155f9e24bf22
       <div className="lists-container">
         <p className="shipping-msg">
           ❗ Please take a note that not articles can be send !
@@ -79,40 +67,6 @@ const Basket = () => {
             </li>
           ))}
         </ul>
-<<<<<<< HEAD
-      </div>
-      <aside>
-        <h4>Abholung</h4>
-
-        <ul>
-          <li>
-            <p>Mo. - Fr. </p> <p>8:00 - 18:00Uhr</p>
-          </li>
-          <li>
-            <p>Sa. </p> <p>9:00 - 14:00Uhr</p>
-          </li>
-          <li>
-            <p>So./Feiertage</p> <p> 9:00 - 13:00Uhr</p>
-          </li>
-        </ul>
-      </aside>
-      <aside>
-        <h4>Adresse</h4>
-
-        <p>Casa Verde</p>
-        <p>Hauptstr. 253 12345 Stadt</p>
-      </aside>
-      
-      <Link  to="/basket/checkout">
-        
-        <h3>
-          Total:
-          {data.basket?.map((item) => item.price).reduce((a, b) => a + b, 0)} $
-        </h3>
-       <p> proceed to Checkout</p>
-       
-      </Link> 
-=======
       </div>{" "}
       <h3>
         Total:
@@ -121,7 +75,6 @@ const Basket = () => {
       <Link className="checkout-link" to="/basket/checkout">
         Proceed to Checkout{" "}
       </Link>
->>>>>>> cd274e9fce30fdf01da27766eea4155f9e24bf22
     </div>
   );
 };
