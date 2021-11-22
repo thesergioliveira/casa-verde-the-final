7398fb1bfbb3850aa93ae0d100866598774cf32c
rubenSinzig <ruben.sinzig@posteo.de>
2021-11-22 12:05:57 +0100

    Technical Consulting -> add background image && remove div for roof

--- client/src/components/TechnicalConsulting.js
+++ client/src/components/TechnicalConsulting.js
@@ -7,62 +7,20 @@ import ContactForm from "./ContactForm";
 import Map from "./Map";
 
 const TechnicalConsulting = () => {
-  const technicMenu = TechnicData.map((obj) => {
-    // const { id, name, link, img } = obj;
-    const { img } = obj;
-    return (
-      // <li key={id}>
-      //    {/* the link is working only using anchor in this example */}
-      //   {/* <Link to={link}>{name}</Link> */}
-      //   <a href={link}>{name}</a>
-      // </li>
-      <img src={img} alt="details" width="100" height="100" />
-    );
-  });
-
-  const componentsMenu = TechnicData.map((obj) => {
-    const { id, name, linkId, img1, img2, img3, img4, img5, img6 } = obj;
-    // tried to put the images into an array and map them, is not working for the  moment
-    // const img = images.map((image) => {
-    //   return (<img src={image} alt="details" width="100" height="100" />
-
-    //   )
-    //});
-    //console.log(img);
-    return (
-      <li key={id}>
-        <h2 id={linkId}>{name}</h2>
-        <div>
-          <img src={img1} alt="details" width="100" height="100" />
-          <img src={img2} alt="details" width="100" height="100" />
-        </div>
-        <div>
-          <img src={img3} alt="details" width="100" height="100" />
-          <img src={img4} alt="details" width="100" height="100" />
-        </div>
-        <div>
-          <img src={img5} alt="details" width="100" height="100" />
-          <img src={img6} alt="details" width="100" height="100" />
-        </div>
-        {/* to do- create pages and adding links to them */}
-        <Link to="/{linkId}">
-          <button>Show more</button>
-        </Link>
-      </li>
-    );
-  });
-
   return (
     <div className="home">
-      <div className="home-top">
-        <div className="roof"></div>
+      <div className="home-top absolut">
+        {/* <div className="roof"></div> */}
         <h1>Casa Verde</h1>
         <p>Technical Consulting</p>
       </div>
-      <ul>{technicMenu}</ul>
-      <a href="#contact">
-        <button>Contact Now!</button>
-      </a>
+
+      <div className="container-room-rental">
+        <img
+          src={process.env.PUBLIC_URL + "../images/technical.png"}
+          alt="Aussenansicht Raumvermietung"
+        />
+      </div>
 
       <Technical />
 
