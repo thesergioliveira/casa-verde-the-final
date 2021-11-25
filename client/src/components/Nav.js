1cd3d35c256d94d570e85d901432867f634945b5
iulianSta <iulianstangithub@gmail.com>
2021-11-25 14:44:17 +0100

    animation fixing issues

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -37,7 +37,7 @@ const Nav = ({ logo }) => {
     return () => {
       window.removeEventListener('scroll', controlNav);
     }
-  }, [])
+  })
 
   //hamburgerMenu
   const navMenu = Menu.map((obj) => {
