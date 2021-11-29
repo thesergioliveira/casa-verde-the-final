855cb7f73989618c5ba0e32f04d8fddc2af3a81d
rubenSinzig <ruben.sinzig@posteo.de>
2021-11-29 10:09:56 +0100

    pull from upstream

--- client/src/components/Shop/PayPal.js
+++ client/src/components/Shop/PayPal.js
@@ -1,29 +1,34 @@
-import { BraintreePayPalButtons, PayPalButtons, PayPalScriptProvider, usePayPalScriptReducer } from "@paypal/react-paypal-js";
+import {
+  BraintreePayPalButtons,
+  PayPalButtons,
+  PayPalScriptProvider,
+  usePayPalScriptReducer,
+} from "@paypal/react-paypal-js";
 import { useState, useEffect } from "react";
 import { PayPalButton } from "react-paypal-button-v2";
 
 export default function PayPal() {
   const [{ options }, dispatch] = usePayPalScriptReducer();
-const [currency, setCurrency] = useState(options.currency);
+  const [currency, setCurrency] = useState(options.currency);
 
-function onCurrencyChange({ target: { value } }) {
-  setCurrency(value);
-  dispatch({
+  function onCurrencyChange({ target: { value } }) {
+    setCurrency(value);
+    dispatch({
       type: "resetOptions",
       value: {
-          ...options,
-          currency: value,
+        ...options,
+        currency: value,
       },
-  });
-}
+    });
+  }
 
-
-
-  return <>
-  {/* {isPending ? <div className="spinner" /> : null} */}
-  <PayPalButton
+  return (
+    <>
+      {/* {isPending ? <div className="spinner" /> : null} */}
+      <PayPalButton
         options={{
-          clientId: "ASHvIIsd34uvS4b7vwdgtcxY7NXGyzyOuXa7YJaZj4cHpZpUtIfK13SCEntdkvK6o26tmNJ73BgDN6R3",
+          clientId:
+            "ASHvIIsd34uvS4b7vwdgtcxY7NXGyzyOuXa7YJaZj4cHpZpUtIfK13SCEntdkvK6o26tmNJ73BgDN6R3",
           currency: "EUR",
         }}
         amount="0.02"
@@ -33,10 +38,6 @@ function onCurrencyChange({ target: { value } }) {
           console.log({ details, data });
         }}
       />
-
-
-</> ;
-            
-        
- 
+    </>
+  );
 }
