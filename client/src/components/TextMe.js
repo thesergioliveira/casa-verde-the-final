79ff5d3eb065633651346d6c18b846b5922aab4d
hamza <darejhamza@gmail.com>
2021-11-30 08:46:31 +0100

    fix login

--- client/src/components/TextMe.js
+++ client/src/components/TextMe.js
@@ -1,8 +1,8 @@
 import React from "react";
-import config  from "../config.json";
-import { ElfsightWidget } from 'react-elfsight-widget';
+import config from "../config.json";
+import { ElfsightWidget } from "react-elfsight-widget";
 
 const TextMe = () => {
   return <ElfsightWidget widgetID={config.APP_ID} />;
-}
+};
 export default TextMe;
