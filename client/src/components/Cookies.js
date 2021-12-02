21656dcc934ce2a517e9c709631da2c6aa4830c0
angelo.p <mrpalaiologos@gmail.com>
2021-12-02 21:55:10 +0100

    fake paypal and cookies pop up ready

--- client/src/components/Cookies.js
+++ client/src/components/Cookies.js
@@ -1,14 +1,21 @@
-import React from 'react'
+import React from "react";
 
 export default function Cookies() {
-    return (
-        <div className="coocie">
+  return (
+    <div className="coocie">
       <input className="checkbox-cb" id="checkbox-cb" type="checkbox" />
-<div className="cookie-bar">
-  <span className="themessage">This website uses cookies to give you an incredible experience. By using this website you agree to the <a href="#">terms</a></span>
-  <span className="themobile">This website uses cookies, <a href="#">learn more</a></span>
-  <label for="checkbox-cb" className="close-cb">x</label>
-</div>
- </div>
-    )
+      <div className="cookie-bar">
+        <span className="themessage">
+          This website uses 🍪cookies🍪 to give you an incredible experience. By
+          using this website you agree to the <a href="#">terms</a>
+        </span>
+        <span className="themobile">
+          This website uses cookies, <a href="#">learn more</a>
+        </span>
+        <label for="checkbox-cb" className="close-cb">
+          x
+        </label>
+      </div>
+    </div>
+  );
 }
