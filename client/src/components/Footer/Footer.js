0b3d50213d54a75a1e5b5defefcaf21ae1b2ca25
iulianSta <iulianstangithub@gmail.com>
2021-12-05 12:34:07 +0100

    adding keys for each child of list links in nav and footer

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -1,20 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import LogoData from "../../JSON/logo.json";
 import FooterData from "../../JSON/footer.json";
+import { v4 as uuidv4 } from "uuid";
 import Cookies from "./Cookies";
 
 // Only About and Contact pages links are working for the moment
 // The other are not created
 
-const Footer = ({ logo }) => {
+const Footer = () => {
+  // Year inside footer
   let now = new Date();
   let year = now.getFullYear();
 
-  const footerMenu = FooterData.map((obj) => {
-    const { id, name, path } = obj;
+  // Logo setup
+  const logo = LogoData.map((obj) => {
+    const { name, path, img } = obj;
     return (
-      <li key={id}>
-        <Link to={path}>{name}</Link>
+      <Link to={path} key={uuidv4()}>
+        <img src={img} alt={name} key={uuidv4()} className={name} />
+      </Link>
+    );
+  });
+
+  const footerMenu = FooterData.map((obj) => {
+    const { name, path } = obj;
+    return (
+      <li key={uuidv4()}>
+        <Link to={path} key={uuidv4()}>{name}</Link>
       </li>
     );
   });
@@ -27,9 +40,11 @@ const Footer = ({ logo }) => {
           <ul>
           {footerMenu}
           </ul>
-        <div className="footer-main">Made by "Team Casa Verde"</div>
+        <div className="footer-main">
+          <p>Made by "Team Casa Verde"</p>
+        </div>
         <div className="footer-secondary">
-          Copyrights reserved Ⓒ {year} Casa Verde
+          <p>Copyrights reserved Ⓒ {year} Casa Verde</p>
         </div>
       </footer>
       <Cookies />
