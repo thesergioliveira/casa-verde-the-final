2aabb7fdac27d66ffcf0f627e8e0f6582de913f7
thesergioliveira <thesergioliveira@gmail.com>
2022-01-24 11:02:32 +0100

    commiting modified files

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import axios from "axios";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import axios from 'axios';
 
-axios.defaults.baseURL = "http://localhost:5000/";
+axios.defaults.baseURL = 'http://localhost:5005/';
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
--- server/app.js
+++ server/app.js
@@ -11,7 +11,7 @@ const cors = require('cors');
 
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: 'http://localhost:5005',
     methods: ['GET', 'POST'],
     credentials: true,
   })
