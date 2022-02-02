fa637fb3d6131f576d45463d9e2dff7c64f28aaf
thesergioliveira <thesergioliveira@gmail.com>
2022-02-02 21:45:02 +0100

    adding untracked files to git log

--- server/app.js
+++ server/app.js
@@ -1,19 +1,19 @@
 // Initializing Dependencies
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var morgan = require("morgan");
-var bodyParser = require("body-parser");
-const session = require("express-session");
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+const session = require('express-session');
 var app = express();
 // Initialize && Use Cors
-const checkoutNodeJssdk = require("@paypal/checkout-server-sdk");
-const cors = require("cors");
+const checkoutNodeJSSDK = require('@paypal/checkout-server-sdk');
+const cors = require('cors');
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST", "PUT", "UPDATE", "DELETE"],
+    origin: 'http://localhost:3000',
+    methods: ['GET', 'POST', 'PUT', 'UPDATE', 'DELETE'],
     credentials: true,
   })
 );
@@ -22,48 +22,48 @@ app.use(
 
 app.use(
   session({
-    key: "token",
+    key: 'token',
     secret: process.env.TOKEN_TEXT,
     resave: false,
     saveUninitialized: false,
   })
 );
 // Setting up Dependencies
-app.use(morgan("dev"));
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 // Alow uploads
-app.use("/uploads", express.static("uploads"));
+app.use('/uploads', express.static('uploads'));
 // mongoDB
 
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 mongoose
   .connect(process.env.DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(console.log("DB is connected 😎"))
+  .then(console.log('DB is connected 😎'))
   .catch((error) => {
     console.log(`There was a problem ${error.message}`);
   });
 
 // Initializing Routes
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/user");
-var adminRouter = require("./routes/admin");
-var ProductRouter = require("./routes/product");
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/user');
+var adminRouter = require('./routes/admin');
+var ProductRouter = require('./routes/product');
 //import middleware
-const middleware = require("./middlewares/middleware");
+const middleware = require('./middlewares/middleware');
 // use routes
 
-app.use("/", indexRouter);
-app.use("/user", usersRouter);
-app.use("/admin", middleware.checkToken, middleware.allowAdmin, adminRouter);
-app.use("/product", ProductRouter);
+app.use('/', indexRouter);
+app.use('/user', usersRouter);
+app.use('/admin', middleware.checkToken, middleware.allowAdmin, adminRouter);
+app.use('/product', ProductRouter);
 
-app.all("*", (req, res, next) => {
+app.all('*', (req, res, next) => {
   next(new Error(`Can't find ${req.originalUrl} on this server`, 404));
 });
 // Exporting App
